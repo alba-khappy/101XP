626f3bcaa702322b3275ef9a345a8822cfa925ca
albina <albinabist1993@mail.ru>
2021-05-20 21:20:31 +0300

    Сверстала за 1 рабочий день

---
+++ js/form.js
@@ -0,0 +1,6 @@
+
+document.getElementsByClassName('form--btn-close')[0].addEventListener('click', function () {
+    var formClose = document.getElementsByClassName('form')[0];
+    formClose.classList.add('close-form');
+})
+
